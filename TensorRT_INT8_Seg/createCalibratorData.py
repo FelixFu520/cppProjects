ef97d3db038d986b873e439dea45956e246978f0
fafu <fa.fu@casivision.com>
2023-08-08 15:03:51 +0800

    创建矫正数据

--- TensorRT_INT8_Seg/createCalibratorData.py
+++ TensorRT_INT8_Seg/createCalibratorData.py
@@ -8,18 +8,21 @@ import mmcv
 
 
 class createCalibratorData(object):
-    def __init__(self, dataset_json: str, npy_path: str = "data.npz", color_type: str = 'color', channel_order: str = 'bgr', percent: float = 0.6):
+    def __init__(self, dataset_json: str, dst_path: str = "./result", color_type: str = 'color', channel_order: str = 'bgr', percent: float = 0.6):
         super().__init__()
 
         self.dataset_json = dataset_json
-        self.npy_path = npy_path
+        self.dst_path = dst_path
         self.percent = percent
         self.color_type = color_type
         self.channel_order = channel_order
 
+        self.calibratorData = list()
+        self.calibratorDataPath = list()
+
     def __call__(self, *args, **kwargs):
         self._load_dataset_json(self.dataset_json)
-        self._save_npy()
+        self._save_image()
 
     def _load_dataset_json(self, json_path: str) -> None:
         dataset_json_dict = mmengine.load(json_path)
@@ -28,7 +31,6 @@ class createCalibratorData(object):
 
         dataset = dataset_json_dict['dataset']
         dataset = dataset[:int(len(dataset) * self.percent)]
-        calibratorData = []
         for index, one_item in enumerate(dataset):
             print(f"parser {index}/{len(dataset)} image ......")
             # 读取图片
@@ -39,26 +41,20 @@ class createCalibratorData(object):
                 img_path = img_path.replace('\\', '/')
                 img_bytes = mmengine.get(img_path)
                 img = mmcv.imfrombytes(img_bytes, flag=self.color_type, channel_order=self.channel_order)
-                # img = img.astype(np.float32)
                 images.append(img)
             image = np.asarray(images)
             image = image.transpose((1, 2, 0))
 
-            # 生成一个样本
-            one_result = dict()
-            one_result['data'] = image
-            one_result['height'] = one_item['height']
-            one_result['width'] = one_item['width']
-            one_result['channels'] = len(images)
-            calibratorData.append(one_result)
+            self.calibratorData.append(image)
+            self.calibratorDataPath.append(os.path.basename(images_path[0]['img_path']))
 
-        self.calibratorData = calibratorData
-
-    def _save_npy(self) -> None:
-        dataDictionary = {}
-        for i, data in enumerate(self.calibratorData):
-            dataDictionary[str(i)] = self.calibratorData[i]
-        np.savez(self.npy_path, **dataDictionary)
+    def _save_image(self) -> None:
+        if os.path.exists(self.dst_path):
+            os.remove(self.dst_path)
+        os.makedirs(self.dst_path, exist_ok=True)
+        for img_path, img in zip(self.calibratorDataPath, self.calibratorData):
+            dst_path = os.path.join(self.dst_path, img_path)
+            cv2.imwrite(dst_path, img)
 
 
 if __name__ == '__main__':
